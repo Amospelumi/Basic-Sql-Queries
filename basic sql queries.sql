4a68e879d60bc79b738c03a6fc6a6c8794aee5a1
Amospelumi <oluwapelumiabiodun45@gmail.com>
2022-01-12 12:54:58 +0100

    Added new querries

--- basic sql queries.sql
+++ basic sql queries.sql
@@ -53,3 +53,57 @@ from employees;
 
 
 -- Write a query to get the first 3 characters of first name from employees table.
+select SUBSTRING(first_name, 1,3)
+from employees;
+
+-- Write a query to calculate 171*214+625.
+select 171*214+625 result;
+
+
+-- Write a query to get the names (for example Ellen Abel, Sundar Ande etc.) of all the employees from employees table. 
+select CONCAT(first_name, " ",  last_name) "Employees Name" from employees;
+
+
+-- Write a query to get first name from employees table after removing white spaces from both side.
+select TRIM(first_name) Trimmed
+from employees;
+
+
+-- Write a query to get the length of the employee names (first_name, last_name) from employees table. 
+select LENGTH(first_name) 'first name', LENGTH(last_name) 'last name', 
+LENGTH(first_name) + LENGTH(last_name) "Total Lengths"
+from employees;
+
+
+-- Write a query to check if the first_name fields of the employees table contains numbers.
+select * from employees
+where first_name REGEXP '[0-9]';
+
+
+-- Write a query to select first 10 records from a table.
+select * from employees limit 10;
+
+
+-- Write a query to get monthly salary (round 2 decimal places) of all employees.
+select CONCAT(first_name, " ", last_name) "Employees", round(salary/12,2) "Monthly salary"
+from employees;
+
+
+-- Write a SQL statement to create duplicate of countries table named country_new with all structure and data.
+create table if not exists pelumi_duplicate
+as select * from countries;
+
+
+-- Write a SQL statement to insert NULL values against region_id column for a row of countries table.
+insert into pelumi_duplicate (COUNTRY_ID,COUNTRY_NAME,REGION_ID) 
+values('A1','AMC',null);
+
+
+-- Write a SQL statement to insert 3 rows by a single insert statement.
+insert into pelumi_duplicate 
+values('A2','AMO', 33),
+		('A2','AMO', 33),
+		('A2','AMO', 33);
+
+
+
